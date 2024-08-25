02ae5ed66d3174de3cc050dcdb6a24e2a6636618
Muzaffer Kılıçaslan <thepitohui@gmail.com>
2024-08-26 00:53:56 +0300

    feat: installed kinde library to authentication, updated navbar with new changes

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,15 @@
 import Link from "next/link";
 import { ArrowRightIcon } from "lucide-react";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 import MaxWidthWrapper from "../MaxWidthWrapper/MaxWidthWrapper";
 import { buttonVariants } from "../ui/button";
 
-const Navbar = () => {
-  const user = undefined;
-  const isAdmin = true;
+const Navbar = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  const isAdmin = user?.email === process.env.ADMIN_EMAIL;
+
   return (
     <>
       <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white bg-white/75 backdrop-blur transition-all">
