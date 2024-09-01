c22f81a12b219af1a273e581d1f5a7c800bfe8b1
Muzaffer Kılıçaslan <thepitohui@gmail.com>
2024-09-02 00:35:18 +0300

    feat: created steps component and used at upload page

--- src/app/configure/layout.tsx
+++ src/app/configure/layout.tsx
@@ -1,10 +1,12 @@
 import { ReactNode } from "react";
 
 import MaxWidthWrapper from "@/components/MaxWidthWrapper/MaxWidthWrapper";
+import Steps from "@/components/Steps/Steps";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <MaxWidthWrapper className="flex-1 flex flex-col">
+      <Steps />
       {children}
     </MaxWidthWrapper>
   );
